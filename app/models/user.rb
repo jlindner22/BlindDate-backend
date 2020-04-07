0599c323f6000801a7380c0a4268ac62fa8490ab
jlindner22 <jenlindner22@gmail.com>
2020-04-06 20:26:50 -0400

    added dependent destroy for user deletion

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    has_many :matches
+    has_many :matches, dependent: :destroy
     has_many :potential_matches, through: :matches
-    has_many :preferences
+    has_many :preferences, dependent: :destroy
     has_secure_password
     # validates :password, confirmation: true
     # validates :username, uniqueness: true
