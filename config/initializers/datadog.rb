068724a4f290480725d6b247c03d3c28232791c5
jlindner22 <jenlindner22@gmail.com>
2020-09-08 17:14:30 -0400

    edited configuration

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,9 +1,10 @@
 # require 'ddtrace'
 
 Datadog.configure do |c|
-    c.env = 'prod'
-    c.service = 'blinddate'
-    c.analytics.enabled = true
+    # c.env = 'prod'
+    # c.service = 'blinddate'
+    # c.analytics.enabled = true
     # This will activate auto-instrumentation for Rails
-    c.use :rails, log_injection: true
+    c.use :rails, service_name: 'blinddate'
+    # log_injection: true
 end
