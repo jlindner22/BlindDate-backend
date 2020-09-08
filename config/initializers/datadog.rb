4a7262efd79ac5eea7fd556bb551f09853ad5a6e
jlindner22 <jenlindner22@gmail.com>
2020-09-08 14:16:36 -0400

    added additional configuration for datadog

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,4 +1,9 @@
+# require 'ddtrace'
+
 Datadog.configure do |c|
+    c.env = 'prod'
+    c.service = 'blinddate'
+    c.analytics.enabled = true
     # This will activate auto-instrumentation for Rails
-    c.use :rails
+    c.use :rails, options
   end
