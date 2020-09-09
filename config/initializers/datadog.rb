f30c2e8047f5ead5a9d06de51c9cf9525dd8def8
jlindner22 <jenlindner22@gmail.com>
2020-09-08 20:51:34 -0400

    testing code removal for logging in production

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,10 +1,4 @@
-# require 'ddtrace'
-
 Datadog.configure do |c|
-    # c.env = 'prod'
-    # c.service = 'blinddate'
-    # c.analytics.enabled = true
     # This will activate auto-instrumentation for Rails
     c.use :rails, service_name: 'blinddate'
-    # log_injection: true
 end
