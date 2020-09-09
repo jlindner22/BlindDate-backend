557c0ad60e027f32e30d2342d13080f54531dd52
jlindner22 <jenlindner22@gmail.com>
2020-09-08 21:22:08 -0400

    testing removing datadog_tracer file

--- config/initializers/datadog_tracer.rb
+++ config/initializers/datadog_tracer.rb
@@ -1,13 +1,13 @@
-Datadog.configure do |c|
-    c.tracer.enabled = true
-    c.tracer.hostname = 'my-agent'
-    c.tracer.port = 8126
-    c.tracer.partial_flush.enabled = false
-    c.tracer.sampler = Datadog::AllSampler.new
+# Datadog.configure do |c|
+#     c.tracer.enabled = true
+#     c.tracer.hostname = 'my-agent'
+#     c.tracer.port = 8126
+#     c.tracer.partial_flush.enabled = false
+#     c.tracer.sampler = Datadog::AllSampler.new
   
-    # OR for advanced use cases, you can specify your own tracer:
-    c.tracer.instance = Datadog::Tracer.new
+#     # OR for advanced use cases, you can specify your own tracer:
+#     c.tracer.instance = Datadog::Tracer.new
   
-    # To enable debug mode:
-    c.diagnostics.debug = true
-  end
+#     # To enable debug mode:
+#     c.diagnostics.debug = true
+#   end
